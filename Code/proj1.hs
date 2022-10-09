b2acf6a5d8cc8988af6429577a318ffc000751de
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2022-10-09 16:08:56 +0100

    Normalizing Polinomial

--- Code/proj1.hs
+++ Code/proj1.hs
@@ -5,4 +5,11 @@ type Symbol = Char
 type Exponent = Int
 
 normalize :: [Monomial] -> [Monomial]
-normalize polinomial = polinomial
+normalize [] = []
+normalize (firstPol:polinomial) = if head [coef | Monomial coef symb exp <- [firstPol]] == 0 then normalize polinomial 
+    else head [
+    Monomial (
+        head [coef + sum [coef2 | monomial <- polinomial, Monomial coef2 symb2 exp2 <- [monomial], symb == symb2, exp == exp2]])
+    symb exp | Monomial coef symb exp <- [firstPol]] 
+    : normalize [monomial | monomial <- polinomial, Monomial coef1 symb1 exp1 <- [firstPol], Monomial coef2 symb2 exp2 <- [monomial],
+    symb1 /= symb2 || exp1 /= exp2]
